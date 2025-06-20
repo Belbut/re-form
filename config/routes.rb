71a2f692f67ca0503a81cf3ba6aba0d2c5977ef5
Belbut <80793911+Belbut@users.noreply.github.com>
2025-06-20 10:46:54 +0100

    feat(user): create basic controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
-
-  resources :user, only: %i[new create]
+  # root 'user#index'
+  resources :users, only: %i[new create]
 end
